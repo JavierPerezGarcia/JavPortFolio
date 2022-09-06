8cb7cd43962a29ee4709011c80456ec8a0f8ba0f
JavPerDev <mojocanario7@gmail.com>
2022-09-07 00:52:34 +0100

    Added a new icon for the accesibility of the web in the hobbies card, new color for the navbar and the sticky, Im doing the 1440x900 media query, next step 1367px resolution or continue with 768x1024 media query

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ const navHobbies = document.querySelector(".nav-hobbies");
 const navProjects = document.querySelector(".nav-projects");
 const navContact = document.querySelector(".nav-contact");
 
+//Reverse about photo
+
+/* const front = document.querySelector(".card-front");
+const back = document.querySelector(".card-back"); */
+
 //Cards elements
 
 const about = document.querySelector(".card-about-me");
@@ -23,6 +28,10 @@ const mail = document.querySelector(".email");
 const linked = document.querySelector(".linkedin");
 const github = document.querySelector(".github");
 
+//Click events
+
+/* front.addEventListener("click", rotate); */
+
 navAbout.addEventListener("click", openCardAbout);
 navEducation.addEventListener("click", openCardEducation);
 navSkills.addEventListener("click", openCardSkills);
@@ -30,11 +39,19 @@ navHobbies.addEventListener("click", openCardHobbies)
 navProjects.addEventListener("click", openCardProjects);
 navContact.addEventListener("click", openCardContact);
 
+about.addEventListener("click", activeNavAbout);
+education.addEventListener("click", activeNavEducation);
+skills.addEventListener("click", activeNavSkills);
+hobbies.addEventListener("click", activeNavHobbies);
+projects.addEventListener("click", activeNavProjects);
+contact.addEventListener("click", activeNavContact);
+
 mail.addEventListener("click", openCardContact);
 linked.addEventListener("click", openCardContact);
 github.addEventListener("click", openCardContact);
 
 const cards = document.querySelectorAll('.card');
+const navs = document.querySelectorAll('.nav');
 
 cards.forEach(card => card.addEventListener('click', transition));
 
@@ -42,31 +59,69 @@ function transition() {
   this.classList.toggle('active');
 }
 
+//Function for rotate the about photo
+
+/* function rotate() {
+  this.classList.toggle('active2');
+} */
+
 //Functions for flip the cards
+
+function activeNavAbout() {
+  navAbout.classList.toggle('active');
+}
+
+function activeNavEducation() {
+  navEducation.classList.toggle('active');
+}
+
+function activeNavSkills() {
+  navSkills.classList.toggle('active');
+}
+
+function activeNavHobbies() {
+  navHobbies.classList.toggle('active');
+}
+
+function activeNavProjects() {
+
+  navProjects.classList.toggle('active');
+}
+
+function activeNavContact() {
+  navContact.classList.toggle('active');
+}
+
 
 function openCardAbout() {
   about.classList.toggle('active');
+  navAbout.classList.toggle('active');
 }
 
 function openCardEducation() {
   education.classList.toggle('active');
+  navEducation.classList.toggle('active');
   /* verifyActive(); */
 }
 
 function openCardSkills() {
   skills.classList.toggle('active');
+  navSkills.classList.toggle('active');
 }
 
 function openCardHobbies() {
   hobbies.classList.toggle('active');
+  navHobbies.classList.toggle('active');
 }
 
 function openCardProjects() {
   projects.classList.toggle('active');
+  navProjects.classList.toggle('active');
 }
 
 function openCardContact() {
   contact.classList.toggle('active');
+  navContact.classList.toggle('active');
 }
 
 function openCardLogo() {
